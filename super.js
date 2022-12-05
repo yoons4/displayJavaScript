fa4c424dc228b0cc79a5e65b514d1129d5322f62
Sangha Yoon <yoons4@spu.edu>
2022-12-05 16:01:48 +0900

    fixed

--- super.js
+++ super.js
@@ -8,4 +8,62 @@ unorderedList.appendChild(listItem1);
 unorderedList.appendChild(listItem2);
 
 const h1 = document.querySelector("h1");
-h1.style.color = "blue"
+h1.style.color = "blue";
+
+async function getUsers() {
+    let url = 'users.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch(error){
+        console.log(error);
+    }
+}
+
+async function renderUsers() {
+    let users = await getUsers();
+    let html = '';
+    users.forEach(user => {
+        let htmlSegment = `<div class= "user">
+        <img src = "${user.profileURL}">
+        <h2>${user.firstName} ${user.lastName}</h2>
+        <div class = "email"><a href = "email:${user.email}">${user.email}</a></div>
+        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+renderUsers();
+
+
+/*
+let response = fetch(url);
+
+fetch(url)
+    .then(response => {
+        //handle the response
+    })
+
+    .catch(error => {
+        //handle the error
+    });
+*/
+
+
+/* Just for the practice
+fetchText();
+
+async function fetchText(){
+    let response = await fetch('/readme.txt');
+    console.log(response.status);
+    console.log(response.statusText);
+
+    if(response.status === 200){
+        let data = await response.text();
+        //handle data
+    }
+}
+*/
